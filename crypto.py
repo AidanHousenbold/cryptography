d310bf59d70b1983a7b21ecba333b19a6ecd133e
Aidan Housenbold <aidan.housenbold@menloschool.org>
2020-09-21 19:08:33 -0700

    Completed Caeser with Corrections

--- crypto.py
+++ crypto.py
@@ -3,13 +3,16 @@
 # Arguments: string, integer
 # Returns: string
 def encrypt_caesar(plaintext, offset):
+    ascii_min = 64
+    ascii_max = 91 
+    alphabet_shift = 26
     encrypt = ""
     for letter in plaintext:
         current = ord(letter)
-        if current > 64 and current < 91:
+        if current > ascii_min and current < ascii_max:
             char = ord(letter) + offset
-            if (char) > 90:
-                char = char - 26 
+            if (char) > ascii_max + 1:
+                char = char - alphabet_shift
             current = char
 
         encrypt = encrypt + chr(current)
@@ -19,13 +22,16 @@ def encrypt_caesar(plaintext, offset):
 # Arguments: string, integer
 # Returns: string
 def decrypt_caesar(ciphertext, offset):
+    ascii_min = 64
+    ascii_max = 91 
+    alphabet_shift = 26
     decrypt = ""
     for letter in ciphertext:
         current = ord(letter)
-        if current > 64 and current < 91:
+        if current > ascii_min and current < ascii_max:
             char = ord(letter) - offset
-            if (char) < 65:
-                char = char + 26 
+            if (char) < ascii_min + 1:
+                char = char + alphabet_shift
             current = char
 
         decrypt = decrypt + chr(current)
